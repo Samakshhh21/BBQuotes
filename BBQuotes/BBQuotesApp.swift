a3dcd0c1f2ef61c08d5a2b21e5e4ef99141ebcfd
samaksh <samakshsangwan@gmail.com>
2025-10-05 12:16:12 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Samaksh Sangwan on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

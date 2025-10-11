e60b98072efe0c75ffe0ba88f64bb899d6b25b54
samaksh <samakshsangwan@gmail.com>
2025-10-11 10:49:08 +0530

    .

--- BBQuotes/Char.swift
+++ BBQuotes/Char.swift
@@ -15,4 +15,32 @@ struct Char : Decodable{
     let portrayedBy : String
     let status : String
     var death : Death?
+    
+    enum CodingKeys: CodingKey {
+        case name
+        case birthday
+        case occupation
+        case images
+        case aliases
+        case portrayedBy
+        case status
+        case death
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.birthday = try container.decode(String.self, forKey: .birthday)
+        self.occupation = try container.decode([String].self, forKey: .occupation)
+        self.images = try container.decode([URL].self, forKey: .images)
+        self.aliases = try container.decode([String].self, forKey: .aliases)
+        self.portrayedBy = try container.decode(String.self, forKey: .portrayedBy)
+        self.status = try container.decode(String.self, forKey: .status)
+        
+        let deathDecoder = JSONDecoder()
+        deathDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deathData = try! Data(contentsOf: Bundle.main.url(forResource: "sampledeath", withExtension: "json")!)        
+        death = try container.decodeIfPresent(Death.self, forKey: .death)
+    }
 }
--- BBQuotes/ViewModel.swift
+++ BBQuotes/ViewModel.swift
@@ -21,10 +21,18 @@ class ViewModel {
     
     private let fetcher = FetchService()
     
-//    var quote: Quote
-//    var character : Char
-//    
-//    init(){
-//        
-//    }
+    var quote: Quote
+    var character : Char
+    
+    init(){
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let quoteData = try! Data(contentsOf: Bundle.main.url(forResource: "samplequote", withExtension: "json")!)
+        quote = try! decoder.decode(Quote.self, from: quoteData)
+        
+        let charData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
+        character = try! decoder.decode(Char.self, from: charData)
+        
+    }
 }
